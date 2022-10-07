d17102c4e8926e0681f9d5010f2eea2492fb2d8b
0xDOG <b.geugies@gmail.com>
2022-10-07 22:40:13 +0200

    Separated Events

--- main.js
+++ main.js
@@ -22,15 +22,29 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
+
+    console.log(`[Loaded] >> Command /${command.data.name}`)
+
     // Set a new item in the collection
     // With the key as the command name and the value as the exported module
     client.commands.set(command.data.name, command);
 }
 
-// When Client Ready, Run this code once:
-client.once('ready', () => {
-    console.log('Ready!');
-})
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+
+    console.log(`[Loaded] >> Event ${event.name}`)
+
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 // Fire when command hits.
 client.on('interactionCreate', async interaction => {
