581d6cb48ea077124abc63719bddfe7defd0991c
0xDOG <b.geugies@gmail.com>
2022-10-13 19:39:17 +0200

    Moved Event to its own file

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const { Client, Collection, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { token, guildId } = require('./config.json');
 
-// Create Client Instance
+/*
+Initialize Client
+ */
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -14,11 +17,18 @@ const client = new Client({
     ],
 });
 
+/*
+Load Commands
+ */
+
 client.commands = new Collection();
+client.devCommands = new Collection();
+client.devId = guildId;
 
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+// Global Commands
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
@@ -30,6 +40,7 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
+// Dev Commands
 const devCommandsPath = path.join(__dirname, 'commands_dev');
 const devCommandFiles = fs.readdirSync(devCommandsPath).filter(file => file.endsWith('.js'));
 
@@ -41,8 +52,12 @@ for (const file of devCommandFiles) {
 
     // Set a new item in the collection
     // With the key as the command name and the value as the exported module
-    client.commands.set(devCommand.data.name, devCommand);
+    client.devCommands.set(devCommand.data.name, devCommand);
 }
+
+/*
+Load Events
+ */
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
@@ -60,28 +75,8 @@ for (const file of eventFiles) {
     }
 }
 
-// Fire when command hits.
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({
-            embeds: [
-                new EmbedBuilder()
-                    .setColor('Red')
-                    .setTitle(`${error.name} - ${error.message}`)
-                    .setDescription('There was an error while executing this command.')
-            ],
-            ephemeral: true
-        })
-    }
-});
+/*
+Login
+ */
 
 client.login(token);
