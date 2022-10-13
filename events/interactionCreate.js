cd4fd59f3fb3a91af8c4ea49d5adcbab19bb6b45
0xDOG <b.geugies@gmail.com>
2022-10-13 19:37:23 +0200

    Moved event from main.js to its own file.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,34 @@
+const {EmbedBuilder} = require("discord.js");
+
 module.exports = {
     name: 'interactionCreate',
-    execute(interaction) {
-        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`)
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
+
+        let command = interaction.client.commands.get(interaction.commandName);
+        let logType = 'Command'
+
+        if (!command) {
+            command = interaction.client.devCommands.get(interaction.commandName)
+            logType = 'devCommand'
+            if (!command) return;
+        }
+
+        console.log(`[${logType}] - ${interaction.user.tag} used /${interaction.commandName} - #${interaction.channel.name}`)
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor('Red')
+                        .setTitle(`${error.name} - ${error.message}`)
+                        .setDescription('There was an error while executing this command.')
+                ],
+                ephemeral: true
+            })
+        }
     },
 };
